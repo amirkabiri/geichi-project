9fcef8673a6773a288dfce37c7dcc7f2dac21f16
Amir Kabiri <akabiridev@gmail.com>
2021-05-12 13:45:07 +0430

    a syntax error added just for deployment test

--- routes/web.php
+++ routes/web.php
@@ -7,36 +7,5 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function (Request $request) {
-    $comment = Comment::find(1);
-    return get_class($comment->sender);
-    return last(explode('\\', $comment->sender_type));
-    return dd(auth('barber')->user());
-    return dd(currentGuard());
-    return auth()->user();
-
-    return \App\Models\Comment::find(1)->sender;
-    return User::find(21)->comments;
-
-
-//    return \Illuminate\Support\Facades\App::version();
-    $user = new User();
-
-//    return User::factory()->create();
-
-    return $user->where('first_name', 'Urban')->get();
-
-    return (int)App::version();
-    return version_compare(\Illuminate\Support\Facades\App::version(), 8.35);
-
-    $user = User::first();
-    if(!$user){
-        $user = User::factory()->create();
-    }
-
-    $token = $user->generateApiToken();
-
-    return [
-        $user,
-        $token
-    ];
+    I am syntax error =|
 });
