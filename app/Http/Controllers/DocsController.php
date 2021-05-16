e28fd02dee17dd1e6efa5ae8f3ee50b599bf737d
Amir Kabiri <akabiridev@gmail.com>
2021-05-16 09:31:39 +0430

    document viewver

---
+++ app/Http/Controllers/DocsController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DocsController extends Controller
+{
+    public function view(){
+        $list = $this->listFiles(base_path() . '/docs');
+        return view('docs', compact('list'));
+    }
+
+    private function listFiles($path){
+        $output = [];
+        if(is_file($path)) return $output;
+
+        foreach (scandir($path) as $item){
+            if($item[0] === '.') continue;
+
+            $item_path = $path . '/' . $item;
+
+            $item_output = [
+                'name' => $item,
+                'is_dir' => is_dir($item_path),
+                'children' => $this->listFiles($item_path),
+            ];
+
+            if(!$item_output['is_dir']){
+                $item_output['content'] = file_get_contents($item_path);
+            }
+
+            $output[] = $item_output;
+        }
+
+        return $output;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,9 @@
 <?php
 
+use App\Http\Controllers\DocsController;
 use App\Models\Comment;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
-Route::get('/', function (Request $request) {
-    return dd(auth()->id());
-    $shop = \App\Models\Shop::with('owner')->find(1);
-    $owner = $shop->owner;
-    $owner = User::find(1);
-    return $owner->isOwnerOfShop(\App\Models\Shop::find(1)) ?? false;
-    return \App\Models\Barber::find(11);
-    return $shop;
-});
+Route::get('/', [DocsController::class, 'view']);
